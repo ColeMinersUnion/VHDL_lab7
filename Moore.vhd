c6a4c1fdff3e97c396b95c7d4fb8e23c990e4d0c
Cole Hansen <130320410+ColeMinersUnion@users.noreply.github.com>
2023-11-30 12:53:55 -0500

    Update Moore.vhd
    PART 2 CHECKOFF

--- Moore.vhd
+++ Moore.vhd
@@ -2,23 +2,65 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity Moore is port(
-	W : IN std_logic_vector(1 downto 0);
-	Reset, CLock, Clock_50: IN std_logic;
-	z, Dflip, flop : OUT std_logic;
-)
+	W : IN std_logic;
+	Reset, Clock, Clock_50: IN std_logic;
+	z : OUT std_logic
+);
 end Moore;
 
 architecture dataflow of Moore is
 
-	component DFlipFlop is 
-		port(Data, Set, CLK, CLK5_50 : IN std_logic;
-				Q : OUT std_logic;
+	component debounce is
+		port(clk, button : IN std_logic;
+				result : OUT std_logic);
 	end component;
-	
-signal state, nextState: std_logic := '0';
+
+type state_03 is (ST0, ST1, ST2);
+signal newClock : std_logic;
+signal state, nextState : state_03;
 	
 begin
+
+	bouncer : debounce port map(Clock_50, Clock, newClock);
+
+	process(newClock, nextState, Reset)
+	begin
+		if (Reset = '0') then
+			state <= ST0;
+		elsif (rising_edge(newClock)) then
+			state <= nextState;
+		end if;
+	end process;
 	
-	
+	process(state, w)
+	begin
+		z <= '0';
+		case state is
+			when ST0 =>
+				Z <= '0';
+				if (w = '1') then
+					nextState <= ST1;
+				else
+					nextState <= ST0;
+				end if;
+			when ST1 => 
+				Z <= '0';
+				if (w = '1') then
+					nextState <= ST2;
+				else
+					nextState <= ST0;
+				end if;
+			when ST2 => 
+				Z <= '1';
+				if(w = '1') then
+					nextState <= ST2;
+				else
+					nextState <= ST0;
+				end if;
+			when others =>
+				Z <= '0';
+				nextState <= ST0;
+		end case;
+	end process;
 	
 end dataflow;
